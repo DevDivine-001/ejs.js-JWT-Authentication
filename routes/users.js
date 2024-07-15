6706bcceef14649aea71a62557b1b4da4f9dbfc0
Oscdivin <divineuwandu84@Gmail.com>
2024-07-15 12:24:06 +0100

    render is add in the api

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,9 @@ const express = require('express')
 
 const router = express.Router()
 // Loin Page
-router.get('/login', (req, res) => res.send('login'))
+router.get('/login', (req, res) => res.render('login'))
 
 // register pages
-router.get('/register', (req, res) => res.send('register'))
+router.get('/register', (req, res) => res.render('register'))
 
 module.exports = router
