3eb91bcaca019b213590ff178c006ce4ddc36077
Oscdivin <divineuwandu84@Gmail.com>
2024-07-19 17:22:00 +0100

    first commit

--- app.js
+++ app.js
@@ -1,23 +1,56 @@
-// const { request } = require("express");
-const expressLayouts = require('express-ejs-layouts')
-const mongoose = require('mongoose')
-const express = require('express')
+// // const { request } = require("express");
+// const expressLayouts = require('express-ejs-layouts')
+// const mongoose = require('mongoose')
+// const express = require('express')
 
-const app = express()
+
+// const app = express()
+
+
+// // Db config
+// const db = require('./config/key').MongoURI
+
+// // Connect to Mongo
+// mongoose.connect(db, { userNewUrlParser: true, useUnifiedTopology: true })
+
+//     .then(() => console.log('mongoose Connected'))
+//     .catch(err => console.log(err))
+// // EJS
+// app.use(expressLayouts)
+// app.set('view engine', 'ejs')
+
+// // Routes
+// app.use("/", require('./routes/index')),
+//     app.use("/users", require('./routes/users'))
+// // app.use("/login", require('./login'))
+
+
+// const PORT = process.env.PORT || 5000
+
+// app.listen(PORT, console.log(`Server is run on port 5000 ${PORT}`))
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const app = express();
+
+// Db config
+const db = require('./config/key').MongoURI;
+
+// Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 // EJS
-app.use(expressLayouts)
-app.set('view engine', 'ejs')
-const db = require('./config/key').MongooseURI
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
 // Routes
-app.use("/", require('./routes/index')),
-    app.use("/users", require('./routes/users'))
-// app.use("/login", require('./login'))
+app.use("/", require('./routes/index'));
+app.use("/users", require('./routes/users'));
 
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, console.log(`Server is run on port 5000 ${PORT}`))
-
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
--- config/key.js
+++ config/key.js
@@ -1,5 +1,5 @@
-const { Mongoose } = require("mongoose");
+
 
 module.exports = {
-    MongooseURI: ''
+    MongoURI: 'mongodb+srv://divine:12345@passportapp.rjgro69.mongodb.net/?retryWrites=true&w=majority&appName=PassportAPP'
 }
