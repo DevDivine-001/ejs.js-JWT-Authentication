1dfd262298c911784b3eba2c709938a6d5e8b941
Oscdivin <divineuwandu84@Gmail.com>
2024-07-20 07:41:21 +0100

    divine

--- models/User.js
+++ models/User.js
@@ -20,5 +20,5 @@ const UserSchema = new mongoose.Schema({
 })
 
 const User = mongoose.model('User', User)
-// export default UserSchema
+export default UserSchema
 module.exports = User
--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ router.get('/register', (req, res) => res.render('register'))
 
 // Register Handle
 router.post('/register', (req, res) => {
-    console.log(req.body)
+    // console.log(req.body)
     // res.send('hello')
     const { name, email, password, password2 } = req.body;
     let errors = []
@@ -22,6 +22,9 @@ router.post('/register', (req, res) => {
         errors.push({ msg: 'Passwords do not match' })
     }
     // Check pass length
+    if (password.length < 6) {
+        errors.push({ msg: 'Password should be at least 6 characters' })
+    }
     if (errors.length > 0) {
         res.render('register', {
             errors,
