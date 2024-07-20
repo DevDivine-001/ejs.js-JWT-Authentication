1b703c49c08508445f5b2229c4c4e22f3ea50b45
Oscdivin <divineuwandu84@Gmail.com>
2024-07-20 01:27:37 +0100

    User.js file is done

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,34 @@ router.get('/login', (req, res) => res.render('login'))
 // register pages
 router.get('/register', (req, res) => res.render('register'))
 
+// Register Handle
+router.post('/register', (req, res) => {
+    console.log(req.body)
+    // res.send('hello')
+    const { name, email, password, password2 } = req.body;
+    let errors = []
+
+    // check required fields
+    if (!name || !email || !password || !password2) {
+        errors.push({ msg: 'Please fill in all fields' })
+    }
+    if (password !== password2) {
+        errors.push({ msg: 'Passwords do not match' })
+    }
+    // Check pass length
+    if (errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        })
+
+
+    } else {
+        res.send('pass')
+    }
+})
+
 module.exports = router
